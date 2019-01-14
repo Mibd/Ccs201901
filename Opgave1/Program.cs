e11bd5fd91bf8aebefd246565924caa0b5a3d9cf
MIBD <mibd@kea.dk>
2019-01-14 15:52:46 +0100

    Opdateret m. kode til opgave 1

--- Opgave1/Program.cs
+++ Opgave1/Program.cs
@@ -10,6 +10,39 @@ namespace Opgave1
     {
         static void Main(string[] args)
         {
+            int a =10;
+            Console.WriteLine(a);
+            a++;
+            Console.WriteLine(a);
+            a--;
+            Console.WriteLine(a);
+            a += 20;
+
+            Console.WriteLine(a);
+
+            Double b = 12.5;
+            Console.WriteLine(b);
+            b++;
+            Console.WriteLine(b);
+            b--;
+            Console.WriteLine(b);
+            b *= 2;
+
+            Console.WriteLine(b);
+
+
+
+
+
+
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+
+                Console.ReadKey();
+            }   
         }
     }
 }
