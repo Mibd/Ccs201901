921b334d76edb4b0c77585af594e76fc706c97ed
MIBD <mibd@kea.dk>
2019-01-15 15:33:42 +0100

    Slut dag 2

---
+++ Struct og Enum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_og_Enum
+{
+    
+    class Program
+    {
+        
+
+
+
+        static void Main(string[] args)
+        {
+
+            {
+                Kortkulør kort1Værdi = Kortkulør.Hjerter;
+
+                Console.WriteLine(kort1Værdi.ToString());
+                Console.WriteLine((int)kort1Værdi);
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ Struct og Enum/Typer/Kortkulør.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_og_Enum
+    {
+    
+        public enum Kortkulør
+        {
+            Spar = 10,
+            Hjerter = 20,
+            Ruder = 30,
+            Klør = 40
+        }
+
+
+    
+
+}
